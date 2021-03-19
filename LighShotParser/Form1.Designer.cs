002acc47a2a905b13d041d2a1a5fbe7271e7705d
Sergey Shumilkin <shumilkin51@list.ru>
2021-03-19 13:23:33 +0300

    LightShotImgLoader v1.0 completed.

---
+++ LighShotParser/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace LighShotImgLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnNextOne = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.btnStartDownl = new System.Windows.Forms.Button();
+            this.btnStopDownl = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(612, 406);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnNextOne
+            // 
+            this.btnNextOne.BackColor = System.Drawing.Color.Red;
+            this.btnNextOne.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNextOne.Font = new System.Drawing.Font("Onyx", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnNextOne.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNextOne.Location = new System.Drawing.Point(630, 12);
+            this.btnNextOne.Name = "btnNextOne";
+            this.btnNextOne.Size = new System.Drawing.Size(105, 45);
+            this.btnNextOne.TabIndex = 1;
+            this.btnNextOne.Text = "Next";
+            this.btnNextOne.UseVisualStyleBackColor = false;
+            this.btnNextOne.Click += new System.EventHandler(this.btnNextOne_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Yellow;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Onyx", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClear.Location = new System.Drawing.Point(630, 63);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(105, 45);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.BackColor = System.Drawing.Color.PowderBlue;
+            this.btnFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFolder.Font = new System.Drawing.Font("Onyx", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnFolder.Location = new System.Drawing.Point(630, 215);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(105, 50);
+            this.btnFolder.TabIndex = 3;
+            this.btnFolder.Text = "Choose Folder";
+            this.btnFolder.UseVisualStyleBackColor = false;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // btnStartDownl
+            // 
+            this.btnStartDownl.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnStartDownl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartDownl.Font = new System.Drawing.Font("Onyx", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStartDownl.Location = new System.Drawing.Point(630, 280);
+            this.btnStartDownl.Name = "btnStartDownl";
+            this.btnStartDownl.Size = new System.Drawing.Size(105, 50);
+            this.btnStartDownl.TabIndex = 4;
+            this.btnStartDownl.Text = "Start downloading";
+            this.btnStartDownl.UseVisualStyleBackColor = false;
+            this.btnStartDownl.Click += new System.EventHandler(this.btnStartDownl_Click);
+            // 
+            // btnStopDownl
+            // 
+            this.btnStopDownl.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnStopDownl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStopDownl.Font = new System.Drawing.Font("Onyx", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStopDownl.Location = new System.Drawing.Point(630, 336);
+            this.btnStopDownl.Name = "btnStopDownl";
+            this.btnStopDownl.Size = new System.Drawing.Size(105, 50);
+            this.btnStopDownl.TabIndex = 5;
+            this.btnStopDownl.Text = "Stop downloading";
+            this.btnStopDownl.UseVisualStyleBackColor = false;
+            this.btnStopDownl.Click += new System.EventHandler(this.btnStopDownl_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Onyx", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(630, 389);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 14);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Count:";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Onyx", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCount.Location = new System.Drawing.Point(668, 389);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(13, 14);
+            this.lblCount.TabIndex = 7;
+            this.lblCount.Text = "0";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblCount.UseMnemonic = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Onyx", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(630, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 14);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Loading now:";
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.Font = new System.Drawing.Font("Onyx", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLoading.Location = new System.Drawing.Point(702, 404);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(22, 14);
+            this.lblLoading.TabIndex = 9;
+            this.lblLoading.Text = "NO";
+            this.lblLoading.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblLoading.UseMnemonic = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(744, 430);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStopDownl);
+            this.Controls.Add(this.btnStartDownl);
+            this.Controls.Add(this.btnFolder);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnNextOne);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "LightShopParser";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnNextOne;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.Button btnStartDownl;
+        private System.Windows.Forms.Button btnStopDownl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ LighShotParser/Form1.cs
@@ -0,0 +1,175 @@
+﻿using CsQuery;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LighShotImgLoader
+{
+    public partial class Form1 : Form
+    {
+        string imgFolder = @"C:\img\";
+        string urlStart = "https://prnt.sc/";
+        char[] symbols = {'1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i',
+            'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        int length = 35;
+        int counter = 0;
+        bool isDownl = false;
+        Random rnd;
+        delegate void countChanged();
+        delegate void isDownlChanged(string s);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            length = symbols.Length;
+            rnd = new Random();
+            // default path
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string folderName = "lightshot_imgs/";
+            imgFolder = Path.Combine(desktop, folderName);
+
+            if(!Directory.Exists(imgFolder))
+                Directory.CreateDirectory(imgFolder);
+
+        }
+
+        // shows one img
+        private void btnNextOne_Click(object sender, EventArgs e)
+        {
+            string curURL = generateNewURL();
+            string imgURL = parseOneURL(curURL);
+            loadIMG(imgURL);
+     
+        }
+
+        // loads one img to form
+        private void loadIMG(string imgURL)
+        {
+            string imgName = imgURL.Split('/').Last();
+            string imgPath = imgFolder + imgName;
+            using (WebClient client = new WebClient())
+            {
+                if (!File.Exists(imgPath)) ;
+                    client.DownloadFile(new Uri(imgURL), imgPath );
+            }
+            pictureBox1.Image = Image.FromFile(imgPath);
+        }
+
+        // getting img URL from html page
+        private string parseOneURL(string url)
+        {
+            string pictureURL;
+            CQ htmlPage = CQ.CreateFromUrl(url);
+
+            pictureURL =htmlPage["body"].Find("img").First().Elements.First().GetAttribute("src");
+           
+            if (pictureURL.Length > 0)
+            {
+                if(pictureURL.StartsWith("//"))
+                {
+                    pictureURL = "https:" + pictureURL;
+                }
+                return pictureURL;
+            }
+            else return "https://im0-tub-ru.yandex.net/i?id=c8606f4d310b3b4ee6bfa8f3cac52c1a&n=13";
+
+        }
+
+        // generates new URLs
+        private string generateNewURL()
+        {
+            StringBuilder urlEnd = new StringBuilder();
+            for(int i=0;i<6;i++)
+            {
+                char rndChar = symbols[rnd.Next(0, length)];
+                urlEnd.Append(rndChar);
+            }
+            string newURL = urlStart + urlEnd;
+            return newURL;
+        }
+
+        // changes folder
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult res = folderBrowserDialog1.ShowDialog();
+            if(res == DialogResult.OK)
+                imgFolder =  folderBrowserDialog1.SelectedPath+ @"\";
+        }
+
+        // clears the panel
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        // async downloading
+        private async void btnStartDownl_Click(object sender, EventArgs e)
+        {
+            lblCount.Text = "0";
+            counter = 0;
+            isDownl = true;
+            await Task.Run( () => downloadImages());
+        }
+
+        // downloading multiple images
+        private void downloadImages()
+        {
+            Invoke(new isDownlChanged(isLoadingChanged), "YES");
+            using (WebClient client = new WebClient())
+            {
+                while (isDownl)
+                {
+                    string curURL = generateNewURL();
+                    string imgURL = parseOneURL(curURL);
+                    string imgName = imgURL.Split('/').Last();
+                    string imgPath = imgFolder + imgName;
+                    {
+                        if (!File.Exists(imgPath))
+                        {
+                            client.DownloadFile(new Uri(imgURL), imgPath);
+                            counter++;
+                            this.Invoke( new countChanged(counterChanged));
+                        }
+                    }
+                }
+            }
+            Invoke(new isDownlChanged(isLoadingChanged),"NO");
+            return;
+        }
+
+        // changing lblTextColor in the main thread
+        private void isLoadingChanged(string s)
+        {
+            if(s == "YES")
+            {
+                lblLoading.ForeColor = Color.DarkRed;
+            }
+            else
+            {
+                lblLoading.ForeColor = Color.Black;
+            }
+            lblLoading.Text = s;
+        }
+
+        // changing lblCountText in the main thread
+        private void counterChanged()
+        {
+            lblCount.Text = counter.ToString();
+        }
+
+        // stops downloading
+        private void btnStopDownl_Click(object sender, EventArgs e)
+        {
+            isDownl = false;
+        }
+    }
+}
